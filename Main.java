716bbeb1d105739539e4a676b8eb10a0c480b4a3
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-01-28 15:05:32 -0500

    Basic JSON Support
    Added basic JSON support, creates JSON file with install directory as an option, allows changing of install directory

--- Main.java
+++ Main.java
@@ -1,32 +1,63 @@
-import org.apache.commons.io.FileUtils;
-import java.io.IOException;
-import java.net.URL;
-import java.io.File;
+import org.json.simple.parser.ParseException;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 import java.lang.String;
-import java.net.URLConnection;
 
 class GUI {
     static String user = System.getProperty("user.name");
-    String downloadsDirectory = "C:/users/"+user+"/Downloads";
+    static JFrame frame = new JFrame("Test GUI");
 
-    public static void main(String[] args) throws IOException{
-        JFrame frame = new JFrame("Test GUI");
+    public static void main(String[] args) throws Exception {
+        JSON.checkJSON();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        JButton button = new JButton("Press");
+        frame.setSize(600, 600);
 
-        button.setBounds(150, 200, 220, 50);
-        button.addActionListener(new ActionListener() {
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+        buttons();
+    }
+
+    static void buttons() {
+        JButton installButton = new JButton("Install Minecraft");
+        installButton.setSize(300, 100);
+
+        installButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Download.downloadZipFromURL("https://www.dropbox.com/s/cqub63z5oeexl37/NoJava.zip?dl=1", "C:/users/"+user+"/Downloads/Minecraft.zip", "C:/users/"+user+"/Downloads/JInstallTest/Minecraft");
+                readJson();
+                Download.downloadZipFromURL("https://www.dropbox.com/s/cqub63z5oeexl37/NoJava.zip?dl=1", JSON.installLocation+"/NoJava.zip", JSON.installLocation);
             }
         });
 
-        frame.add(button);
-        frame.setVisible(true);
+        JButton quitButton = new JButton("Quit");
+        quitButton.setSize(300, 100);
+        quitButton.setBounds(0,100,300,100);
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.add(installButton);
+        frame.add(quitButton);
     }
 
+
+
+    public static void readJson() {
+        try {
+            JSON.readJSON();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
+
